ecda54a4079f30e51767ff654dfd7674ccbb87a5
1f604 <1f604@users.no.reply.github.com>
2017-02-02 00:10:55 +0000

    finished implementation
    All features done:
    - Opening and saving encrypted files
    - Watching for unsaved changes
    - Enabling undo

--- main.py
+++ main.py
@@ -15,21 +15,39 @@ import base64
 
 conf = "dropass.config"
 
-
-
-
-
-
-
-
-
-
 def createnewconfig():
     global filepath
     filepath = input("Enter the full path+filename of your password file (will be created if not already exists): ")
     #password = input("Enter your master password: ")# Yeah, don't do this.
     with open(conf, "w") as f:
         f.write(filepath)  # encrypt some lines using the password
+
+def saveas(event=None):
+    global text
+    global dirty
+    plaintext = text.get("1.0", "end-1c")
+    with open(filepath, "wb") as f:
+        f.write(encrypt(plaintext, password))  # encrypt some lines using the password
+    dirty = False
+    frame.configure(bg='teal')
+
+
+def checkUnsavedChanges(event=None):
+    # check if saving
+    # if not:
+    if dirty:
+        if messagebox.askyesno("Exit", "THERE ARE UNSAVED CHANGES! Do you want to quit the application?"):
+            if messagebox.askokcancel("Exit", "THERE ARE UNSAVED CHANGES!! ARE YOU SURE you want to quit the application?"):
+                win = tk.Toplevel()
+                win.title('warning')
+                message = "This will delete stuff"
+                tk.Label(win, text=message).pack()
+                tk.Button(win, text='No!', command=win.destroy).pack()
+                tk.Button(win, text='Delete', command=root.destroy).pack()
+                tk.Button(win, text='Noo!', command=win.destroy).pack()
+                tk.Button(win, text='Don\'t delete!!', command=win.destroy).pack()
+    else:
+        root.destroy()
 
 #1. Check if config exists. If not then create new config
 if os.path.isfile(conf):
@@ -51,47 +69,44 @@ if not os.path.isfile(filepath):
     print("File not found. Creating new file.")
     password = input("Enter your new master password: ")
     with open(filepath, "wb") as f:
-        f.write(encrypt("Enter your passwords in this file 23123",password))#encrypt some lines using the password
+        f.write(encrypt("Enter your passwords in this file.",password))#encrypt some lines using the password
 else:
     print("Found existing file.")
     password = input("Enter the master password for the file: ")
 with open(filepath,"rb") as f:
     s = f.read()
 contents = decrypt(s,password) #throws exception if this fails
-print(contents)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 root=tk.Tk("Passwords Editor - DroPass")
-
-frame = tk.Frame(root, bg='brown')
+frame = tk.Frame(root, bg='teal')
 frame.pack(fill='both', expand='yes')
 text=tkst.ScrolledText(
     master = frame,
     wrap   = 'word',  # wrap text at full words only
     width  = 80,      # characters
     height = 30,      # text lines
-    bg='beige'        # background color of edit area
+    bg='beige',        # background color of edit area
+    undo=True
 )
 text.grid()
-def saveas():
-    global text
-    t = text.get("1.0", "end-1c")
-    savelocation=filedialog.asksaveasfilename()
-    file1=open(savelocation, "w+")
-    file1.write(t)
-    file1.close()
-button=tk.Button(root, text="Save", command=saveas)
+def callback(event):
+    global dirty
+    frame.configure(bg='brown')
+    dirty = True
+    text.edit_modified(0) #IMPORTANT - or <<Modified>> will not be called later.
+text.bind('<<Modified>>', callback)
+button=tk.Button(root, text="Save", command=saveas,   padx=8, pady=8)
+button.pack()
+# the padx/pady space will form a frame
+text.pack(fill='both', expand=True, padx=8, pady=8)
+
+text.insert('insert', contents)
+dirty = False
+frame.configure(bg='teal')
+
+root.bind_all("<Control-q>", checkUnsavedChanges)
+root.bind_all("<Control-s>", saveas)
+
+root.protocol('WM_DELETE_WINDOW', checkUnsavedChanges)  # root is your root window
+
 root.mainloop()
