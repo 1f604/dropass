399d304a41db4b4e0fe0940db51cd433d6f98cfa
1f604 <1f604@users.noreply.github.com>
2018-01-20 22:24:49 +0000

    improved file modified detection

--- main.py
+++ main.py
@@ -2,37 +2,42 @@
 #1. Checks for config file. If config file not exist or not in right format, create new config file by asking for path of password file and the master password. Goto 2
 #If password file not exist or cannot be decrypted with password supplied, again create new config file.
 #2. When the user clicks on save button, the contents of the file is encrypted with the master password and then saved at path.
-
-import sys
+ 
 from cryptotest import encrypt, decrypt
 import tkinter as tk
-from tkinter import messagebox
-from tkinter import * 
+from tkinter import messagebox 
 import tkinter.scrolledtext as tkst
 import os.path
-import getpass 
-import base64
+import getpass  
+import os
 
 conf = "dropass.config"
 teal='#008080'
 brown='#800000'
-
+filepath = ""
+curSavedFileContents = ""
 
 def createnewconfig():
     global filepath
-    filepath = input("Enter the full path+filename of your password file (will be created if not already exists): ")
+    filepath = input("Enter the full path+filename of your password file (will be created if not already exists, leave empty to create default file in data dir): ")
+    if filepath == "":
+        filepath = "data/passwords.encrypted"
     with open(conf, "w") as f:
         f.write(filepath)  # encrypt some lines using the password
 
-def saveas(event=None):
+def getTextboxContents():
     global text
+    return text.get("1.0", "end-1c")
+
+def saveas(event=None):
     global dirty
-    plaintext = text.get("1.0", "end-1c")
+    global curSavedFileContents
+    plaintext = getTextboxContents()
+    curSavedFileContents = plaintext
     with open(filepath, "wb") as f:
         f.write(encrypt(plaintext, password))  # encrypt some lines using the password
     dirty = False
     frame.configure(bg=teal)
-
 
 def checkUnsavedChanges(event=None):
     # check if saving
@@ -52,22 +57,42 @@ def checkUnsavedChanges(event=None):
         root.destroy()
 
 def ismodified(event):
-        global dirty
+    global dirty
+    plaintext = getTextboxContents()
+    if plaintext != curSavedFileContents:
         frame.configure(bg=brown)
-        dirty = True
-        text.edit_modified(0)  # IMPORTANT - or <<Modified>> will not be called later.
+        dirty = True        
+    else:
+        frame.configure(bg=teal)
+        dirty = False
+    text.edit_modified(0)  # IMPORTANT - or <<Modified>> will not be called later.
 
 # Select all the text in textbox
 def select_all(event):
-    text.tag_add(SEL, "1.0", END)
+    text.tag_add(tk.SEL, "1.0", tk.END)
     return 'break'
 # Select current line in textbox
 def select_line(event): 
-    current_line = text.index(INSERT)
-    text.tag_add(SEL, "insert linestart", "insert lineend+1c")
+    tk.current_line = text.index(tk.INSERT)
+    text.tag_add(tk.SEL, "insert linestart", "insert lineend+1c")
     return 'break'
     #after(interval, self._highlight_current_line)
 
+def askpassword():
+    password1 = getpass.getpass("Enter your new master password: ")
+    password2 = getpass.getpass("Enter your new master password again: ")
+    if (password1 != password2):
+        print("Passwords do not match, file not created, try again.") #don't create the file if the passwords don't match
+        exit(1)
+    if not password1:
+        print("YOU MUST ENTER A PASSWORD! File not created, try again.")
+        exit(1)
+    return password1
+
+def createnewfile(password):
+    os.makedirs(os.path.dirname(filepath), exist_ok=True)
+    with open(filepath, "wb") as f:
+        f.write(encrypt("Enter your passwords in this file.",password)) #encrypt some lines using the password
 
 
 if __name__ == "__main__":
@@ -88,19 +113,15 @@ if __name__ == "__main__":
 
     if not os.path.isfile(filepath):
         print("File not found. Creating new file.")
-        password1 = getpass.getpass("Enter your new master password: ")
-        password2 = getpass.getpass("Enter your new master password again: ")
-        if (password1 != password2):
-            print("Passwords do not match, file not created, try again.") #don't create the file if the passwords don't match
-            exit(1)
-        with open(filepath, "wb") as f:
-            f.write(encrypt("Enter your passwords in this file.",password1))#encrypt some lines using the password
+        password = askpassword()
+        createnewfile(password)
     else:
         print("Found existing file.")
         password = getpass.getpass("Enter the master password for the file: ")
     with open(filepath,"rb") as f:
         s = f.read()
     contents = decrypt(s,password) #throws exception if this fails
+    curSavedFileContents = contents
 
     #3. Main window setup
     root=tk.Tk()
